596f6cfbde0e44ce76a0ecac3e1e9106cc9283e8
Kseniia1992 <ksenia1992s@gmail.com>
2014-10-14 13:47:41 +0300

    8 queens

---
+++ Queens.java
@@ -0,0 +1,57 @@
+package pac;
+
+public class Queens {
+	
+	 static int n=8;
+	 static int[] board=new int[n];
+	 static int solution=0;
+	 
+	    public static boolean isSafe(int r, int c) {	        
+	        for (int i = 0; i < r; i++) {
+	        	//check by vertical, diagonals 
+	            if (board[i] == c || (i - r) == (board[i] - c) ||(i - r) == (c - board[i])) 
+	            {
+	                return false; //not safe
+	            }
+	        }
+	        return true; //safe cell for queen
+	    }
+	 
+	    public static void printQueens() {
+	    	System.out.println("\n #" + (++solution));	       
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < n; j++) {
+	                if (board[i] == j) {
+	                    System.out.print("1 ");
+	                } else {
+	                    System.out.print("- ");
+	                }
+	            }
+	            System.out.println();
+	        }
+	        System.out.println();
+	    }
+	    
+	 //checking all rows and cells to set queen
+	    public static void setQueen(int r, int n) {
+	        for (int c = 0; c < n; c++) {
+	            if (isSafe(r, c)) {
+	            	board[r] = c; 
+	                if (r == 7) { 
+	                    printQueens();
+	                } else {
+	                    setQueen(r + 1, n); // only one queen in row; go to next row
+	                }
+	            }
+	        }
+	    }
+	 
+	    public static void callsetQueen() {
+	        setQueen(0, board.length);
+	    }
+	 
+	    public static void main(String args[]) {
+	    	callsetQueen();	      
+	    }
+
+}
